4ba53b918cecd17527d77772701c32063b92e4f5
Luis <webdeveloperpr@gmail.com>
2018-03-13 17:01:11 -0400

    git add re-add docs

--- config/constants.js
+++ config/constants.js
@@ -1,12 +1,18 @@
 const path = require('path');
+
+// github deployment
 const DIST_DIR = path.join(__dirname, '../docs');
 const ENTRY_FILE = path.join(__dirname, '../index.js');
 const TEMPLATES_DIR = path.join(__dirname, 'templates');
 const DIST_DIR_INDEX_FILE = path.join(DIST_DIR, 'index.html');
+
+// npm
 const NPM_DIST_DIR = path.join(__dirname, '../dist');
 const NPM_ENTRY_FILE = path.join(__dirname, '../src');
+
 // templates
 const TEMPLATE_FILE = path.join(TEMPLATES_DIR, 'template.html');
+
 // prod
 const VENDOR_LIBS = [
   'babel-polyfill',
